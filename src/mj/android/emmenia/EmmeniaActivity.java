b94b4f9f35023b1d66b65b9f9f5cee66a7f60989
MontyJee <MontyJee@gmail.com>
2012-04-25 21:26:49 +0400

    files

--- src/mj/android/emmenia/EmmeniaActivity.java
+++ src/mj/android/emmenia/EmmeniaActivity.java
@@ -2,12 +2,36 @@ package mj.android.emmenia;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 public class EmmeniaActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	
+	LinearLayout im;
+
+	/** Called when the activity is first created. */
+	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		im = (LinearLayout) findViewById(R.id.splashscreen); 
+		
+		Animation a = AnimationUtils.loadAnimation(this, R.anim.alpha);
+		a.reset();
+		a.setAnimationListener(new Animation.AnimationListener() {
+			public void onAnimationStart(Animation a) {
+				//
+			}
+			public void onAnimationRepeat(Animation a) {
+				//
+			}
+			public void onAnimationEnd(Animation a) {
+				//im.setBackgroundResource(R.drawable.rrr);
+			}
+		});
+
+		im.startAnimation(a);
+	}
 }
