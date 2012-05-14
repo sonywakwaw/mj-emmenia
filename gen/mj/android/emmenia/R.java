ebb681877c5e2191e189ae5f92658f66b34fb111
MontyJee <MontyJee@gmail.com>
2012-05-14 21:28:58 +0400

    loosen files

--- gen/mj/android/emmenia/R.java
+++ gen/mj/android/emmenia/R.java
@@ -14,30 +14,46 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int bg=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int s01=0x7f020002;
-        public static final int s02=0x7f020003;
-        public static final int s03=0x7f020004;
-        public static final int s04=0x7f020005;
-        public static final int s05=0x7f020006;
-        public static final int splash=0x7f020007;
+        public static final int b_add=0x7f020000;
+        public static final int b_delete=0x7f020001;
+        public static final int b_edit=0x7f020002;
+        public static final int b_exit=0x7f020003;
+        public static final int b_prefs=0x7f020004;
+        public static final int b_stat=0x7f020005;
+        public static final int bg=0x7f020006;
+        public static final int e01=0x7f020007;
+        public static final int e02=0x7f020008;
+        public static final int e03=0x7f020009;
+        public static final int e04=0x7f02000a;
+        public static final int e05=0x7f02000b;
+        public static final int e06=0x7f02000c;
+        public static final int e07=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int s01=0x7f02000f;
+        public static final int s02=0x7f020010;
+        public static final int s03=0x7f020011;
+        public static final int s04=0x7f020012;
+        public static final int s05=0x7f020013;
+        public static final int splash=0x7f020014;
     }
     public static final class id {
         public static final int Date=0x7f080003;
-        public static final int DescText=0x7f080004;
+        public static final int Days=0x7f080007;
+        public static final int DescText=0x7f080005;
         public static final int Icon=0x7f080006;
-        public static final int Title=0x7f080007;
-        public static final int add=0x7f08000b;
+        public static final int Title=0x7f080008;
+        public static final int add=0x7f08000d;
         public static final int butCancel=0x7f080002;
         public static final int butSave=0x7f080001;
         public static final int buttons=0x7f080000;
-        public static final int delete=0x7f08000a;
-        public static final int deleteAll=0x7f08000c;
-        public static final int edit=0x7f080009;
-        public static final int exit=0x7f08000d;
-        public static final int gallery=0x7f080005;
-        public static final int splashscreen=0x7f080008;
+        public static final int delete=0x7f08000c;
+        public static final int edit=0x7f08000b;
+        public static final int exit=0x7f080010;
+        public static final int gallery=0x7f080004;
+        public static final int list=0x7f080009;
+        public static final int settings=0x7f08000f;
+        public static final int splashscreen=0x7f08000a;
+        public static final int stat=0x7f08000e;
     }
     public static final class layout {
         public static final int add_update=0x7f030000;
@@ -50,15 +66,17 @@ public final class R {
         public static final int main_menu=0x7f070001;
     }
     public static final class string {
-        public static final int add=0x7f05000e;
+        public static final int Settings=0x7f05000d;
+        public static final int Statistic=0x7f05000c;
+        public static final int add=0x7f05000f;
         public static final int app_name=0x7f050001;
         public static final int cancel=0x7f05000a;
         public static final int datefield=0x7f050004;
+        public static final int days=0x7f050010;
         public static final int delete=0x7f050009;
-        public static final int deleteAll=0x7f05000c;
         public static final int edit=0x7f050008;
         public static final int entertext=0x7f050006;
-        public static final int exit=0x7f05000d;
+        public static final int exit=0x7f05000e;
         public static final int hello=0x7f050000;
         public static final int icon=0x7f05000b;
         public static final int iconfield=0x7f050005;
